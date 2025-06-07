d4f52703b46412b94f7cdc0eedf292f83dec396f
radeqq007 <radeqq007@interia.pl>
2025-06-07 11:05:16 +0200

    feat: modify request headers

--- internal/proxy/router.go
+++ internal/proxy/router.go
@@ -10,6 +10,17 @@ func NewRouter(cfg *config.Config) *http.Server {
 	server := &http.Server{
 		Addr: ":" + strconv.Itoa(cfg.Port),
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			// Add headers
+			for key, value := range cfg.Headers.Add {
+				w.Header().Set(key, value)
+			}
+
+			// Remove headers
+			for _, key := range cfg.Headers.Remove {
+				w.Header().Del(key)
+			}
+
 			for _, route := range cfg.Routes {
 				if len(r.URL.Path) >= len(route.PathPrefix) && r.URL.Path[:len(route.PathPrefix)] == route.PathPrefix {
 					proxy := newProxy(route.Target)
