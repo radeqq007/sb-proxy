d491a292342271c4ef50d3dc0ff6a75842fffae4
radeqq007 <radeqq007@interia.pl>
2025-06-07 15:58:49 +0200

    feat: use flag to set config path

--- cmd/sb-proxy/main.go
+++ cmd/sb-proxy/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"sb-proxy/internal/config"
 	"sb-proxy/internal/proxy"
 )
 
 func main() {
-	cfgPath := "sb-config.json"
-	cfg, err := config.Load(cfgPath)
+	cfgPath := flag.String("c", "", "Path to config file")
+	flag.Parse()
+
+	if *cfgPath == "" {
+		*cfgPath = "sb-config.json"
+	}
+
+	cfg, err := config.Load(*cfgPath)
 	if err != nil {
 		panic(err)
 	}
